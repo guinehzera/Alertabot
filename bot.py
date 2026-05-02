86d197b9a9cf1d313d9de9bc5112707daa172d1d
guinehzera <guinespirra@hotmail.com>
2026-05-02 04:06:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,13 @@ alertas_enviados = {}
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
         self.wfile.write(b"Bot rodando!")
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
     def log_message(self, format, *args):
         pass
 
